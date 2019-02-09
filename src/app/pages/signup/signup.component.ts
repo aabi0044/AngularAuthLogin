fbc28f89c624422000c38e134ea6ccbd328d008d
Mabdulah <Mabdulah596@hotmail.com>
2019-02-09 22:55:27 +0500

    update

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -10,13 +11,14 @@ export class SignupComponent implements OnInit {
 name;
 email;
 password;
-  constructor(private auth:AuthService) { }
+  constructor(private auth:AuthService, private router:Router) { }
 
   ngOnInit() {
   }
 signup(){
 this.auth.signup(this.email,this.password).then(res=>{
   console.log("userCreated");
+  this.router.navigate(['/login']);
 })
 }
 }
